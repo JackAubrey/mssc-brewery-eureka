b963a2f50baac8fd2aafdf820917fc578c4c3df8
danilo <d.cividin@hotmail.it>
2024-04-04 18:13:55 +0200

    enabled context propagation in order to works with reactor

--- src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -3,12 +3,14 @@ package guru.sfg.brewery.eureka.msscbreweryeureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import reactor.core.publisher.Hooks;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class MsscBreweryEurekaApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(MsscBreweryEurekaApplication.class, args);
 	}
 
